978ec185c83e4768af7c8448d34041a68b7ef328
sharkus <dr.benway@posteo.de>
2023-04-05 12:41:00 +0200

    Remove is.vim

--- bin/bin/vim_plugins.py
+++ bin/bin/vim_plugins.py
@@ -24,7 +24,6 @@ plugins = {
         "fugitive": "https://github.com/tpope/vim-fugitive.git",
         "gitgutter": "https://github.com/airblade/vim-gitgutter.git",
         "gundo": "https://github.com/sjl/gundo.vim.git",
-        "is": "https://github.com/haya14busa/is.vim.git",
         "javascript": "https://github.com/pangloss/vim-javascript.git",
         "jedi-vim": "https://github.com/davidhalter/jedi-vim.git",
         "lualine": "https://github.com/nvim-lualine/lualine.nvim.git",
