959c42bce902abc53905435784aafe5930892ebd
sharkus <dr.benway@posteo.de>
2024-03-17 11:52:49 +0100

    Adapt loading of FZF bindings

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
     if type -q fzf_key_bindings
-        fzf_key_bindings
+        fzf --fish | source
     end
 
     # Use vi mode with emacs key bindings in insert mode
