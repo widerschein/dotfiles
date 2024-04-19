2160b4142af51254d55cdc4c282677ce49b70316
sharkus <dr.benway@posteo.de>
2024-04-19 09:25:52 +0200

    Move binding and cleanup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,10 @@
+# Remove greeter text
 set -U fish_greeting
-
-bind -M insert -m default jk repaint
 
 # Colors
 set -g fish_color_user yellow
 
-
-# FZF
+# fzf
 set -g FZF_DEFAULT_COMMAND 'rg --hidden --files'
 
 # Autojump
@@ -22,12 +20,3 @@ if not test -d $bass_dir
     git clone https://github.com/edc/bass.git $bass_dir
     make -C $bass_dir install
 end
-
-# Prompt
-#set -g __fish_git_prompt_shorten_branch_len 10
-#set -g __fish_git_prompt_describe_style describe
-#set -g __fish_git_prompt_show_informative_status 1
-
-# Aliases
-
-
--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -4,4 +4,7 @@ function fish_user_key_bindings
     # Use vi mode with emacs key bindings in insert mode
     fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase insert
+
+    # Enter normal mode
+    bind -M insert -m default jk repaint
 end
